d4224a794e91b96e6f4352da4a9c16d2f7e5a90a
Tung Yu Chen <james19900311@gmail.com>
2022-10-07 17:28:11 -0700

    change resume

--- portfolio-react-app/src/components/Navbar.jsx
+++ portfolio-react-app/src/components/Navbar.jsx
@@ -77,7 +77,7 @@ const Navbar = () => {
             </li>
             <li className="w-[160px] h-[60px] flex justify-between items-center  ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]" >
                 <a className="flex justify-between items-center w-full text-gray-300"
-                 href="https://drive.google.com/file/d/1H3kJVp4sWz7Ckh51WbS3f4MVNdDGENby/view?usp=sharing" target="_blank" rel="noreferrer">
+                 href="https://drive.google.com/file/d/1mcJB9BvkbmeJ_WeHqMN4QdB6UZeTqdTX/view?usp=sharing" target="_blank" rel="noreferrer">
                     Resume <BsFillPersonLinesFill size={30}/></a>
             </li>
         </ul>
